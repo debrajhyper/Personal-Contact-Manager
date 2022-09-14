3e2dc355894cbddc5531032d0225877252fc1ba0
Debraj Karmakar <debrajkarmakar010@gmail.com>
2022-09-14 17:18:44 +0530

    remove all env file

--- PCM/pcm-frontend/src/api/HomeAPI.js
+++ PCM/pcm-frontend/src/api/HomeAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 
-const BASE_URL = process.env.BACKEND_BASE_URL;
+// const BASE_URL = "http://localhost:1010";
+const BASE_URL = "https://personal-contact-manager-app.herokuapp.com";
 export const LOGIN_URL = "/generate-token";
 export const SEND_OTP_URL = "/send-otp";
 export const VERIFY_OTP_URL = "/verify-otp";
