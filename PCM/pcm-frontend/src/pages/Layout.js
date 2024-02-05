eec778e41733ff0bea1641b17cb6689e698cb25b
Debraj <debrajkarmakar010@gmail.com>
2024-02-05 12:54:24 +0530

    chor: Add vercel speed insights in app layout

--- PCM/pcm-frontend/src/pages/Layout.js
+++ PCM/pcm-frontend/src/pages/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const Layout = () => {
     return (
         <main className="App_Main">
             <Outlet />
+            <SpeedInsights />
         </main>
     )
 }
