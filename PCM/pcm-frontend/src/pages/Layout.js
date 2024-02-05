81ea76de9a3c434108bfe381b6ea3390d6a1e3aa
Debraj <debrajkarmakar010@gmail.com>
2024-02-05 12:59:05 +0530

    fix: deployment issue resolve for vercel speed insight

--- PCM/pcm-frontend/src/pages/Layout.js
+++ PCM/pcm-frontend/src/pages/Layout.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const Layout = () => {
     return (
         <main className="App_Main">
             <Outlet />
-            <SpeedInsights />
         </main>
     )
 }
